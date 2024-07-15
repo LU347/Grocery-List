5a5868c0f96ccb2e8b0997162097d86b970fa98d
LU347 <103535661+LU347@users.noreply.github.com>
2024-07-14 23:36:04 -0500

    Navbar and Page Routes setup

---
+++ src/app/lists/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="pt-24 container mx-auto px-4 py-8">
+            <p>Users should be able to view their lists here</p>
+        </div>
+    );
+}
---
+++ src/app/ui/navbar.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../../../public/logo.svg";
+import Menu from "../../../public/menu.svg";
+import CloseButton from "../../../public/close.svg";
+import { useState } from "react";
+
+export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleNav = () => {
+        setMenuOpen(!menuOpen);
+    }
+
+    return (
+    <nav className="fixed w-full h-24 shadow-xl bg-white">
+        <div className="flex justify-between items-center h-full w-full px-4 2xl:px-16">
+            <Link href='/' className="flex items-center">
+            <Image
+                src={Logo}
+                alt="Logo"
+                width="75"
+                height="75"
+                className="cursor-pointer filter brightness-0"
+                priority
+            />
+            <h1>Grocery Buddy</h1>
+            </Link>
+            <div className="hidden sm:flex">
+                <ul className="hidden sm:flex">
+                    <Link href="/">
+                        <li className="ml-10 uppercase hover:border-b text-xl">Home</li>
+                    </Link>
+                    <Link href="/lists">
+                        <li className="ml-10 uppercase hover:border-b text-xl">Lists</li>
+                    </Link>
+                    <Link href="/account">
+                        <li className="ml-10 uppercase hover:border-b text-xl">Account</li>
+                    </Link>
+                </ul>
+            </div>
+            <div onClick={handleNav} className="sm:hidden cursor-pointer pl-24">
+                <Image
+                    src={Menu}
+                    alt="menu icon"
+                    width="25"
+                    height="25"
+                    className="md:hidden filter brightness-0"
+                />
+            </div>
+        </div>
+        <div className={
+            menuOpen
+            ? "fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+            : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+        }
+        >
+        <div className="flex w-full items-center justify-end">
+            <div onClick={handleNav} className="cursor-pointer">
+                <Image
+                    src={CloseButton}
+                    alt="close menu icon"
+                    width="25"
+                    height="25"
+                    className="filter brightness-0"
+                />
+            </div>
+        </div>
+            <div className="flex-col py-4">
+                <ul>
+                    <Link href="/">
+                        <li
+                         onClick={() => setMenuOpen(false)}
+                         className="py-4 cursor-pointer"
+                        >
+                            Home
+                        </li>
+                    </Link>
+                    <Link href="/lists">
+                        <li 
+                         onClick={() => setMenuOpen(false)}
+                         className="py-4 cursor-pointer"
+                        >
+                            Lists
+                        </li>
+                    </Link>
+                    <Link href="/account">
+                        <li 
+                        onClick={() => setMenuOpen(false)}
+                        className="py-4 cursor-pointer"
+                        >
+                            Account
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+    );
+}
